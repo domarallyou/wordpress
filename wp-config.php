0fd33cc668262087d831acc68f71d621325b8495
KhanhLong <105224221+domarallyou@users.noreply.github.com>
2022-11-14 22:06:46 +0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'Group3' );
+define( 'DB_NAME', 'freedb_Group3' );
 
 /** Database username */
-define( 'DB_USER', 'ot' );
+define( 'DB_USER', 'freedb_TMDTT' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'k!#ka8JN6ZwuM#f' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'sql.freedb.tech' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
